6e30b5c796a58db7f63172874f77161a4a0d84e8
sabe-0524 <soichiro.0524@icloud.com>
2026-02-21 20:12:16 +0900

    Revert "LIN-304: protocol-ws契約型を追加 (#471)"
    This reverts commit f0db718449e3c2add70d92bad80681236c2e9e85.

--- rust/src/contracts/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod protocol_ws;
-
-pub use protocol_ws::{
-    AckPayload, AckStatus, AuthPayload, EventPayload, Frame, HelloPayload, Op, SendMessagePayload,
-};
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,3 @@
-pub mod contracts;
-
 use axum::{
     extract::ws::{Message, WebSocket, WebSocketUpgrade},
     response::IntoResponse,
@@ -15,7 +13,8 @@ async fn main() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "info".into()),
         )
         .init();
 
