f0db718449e3c2add70d92bad80681236c2e9e85
sabe-0524 <167090960+sabe-0524@users.noreply.github.com>
2026-02-21 11:15:51 +0900

    LIN-304: protocol-ws契約型を追加 (#471)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,5 @@
+pub mod contracts;
+
 use axum::{
     extract::ws::{Message, WebSocket, WebSocketUpgrade},
     response::IntoResponse,
@@ -13,8 +15,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "info".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
         )
         .init();
 
