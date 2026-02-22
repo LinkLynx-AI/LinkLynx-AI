7c64f5547793b6bc2b40e49a7f09fa2d35893dfc
melswonder <a07054617811@icloud.com>
2026-02-23 00:31:10 +0900

    fix: format

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -13,8 +13,7 @@ async fn main() {
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "info".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
         )
         .init();
 
